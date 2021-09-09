47898778bfcc196669d46f5682cc343ba6e8a106
Anna <54678969+Lamia027@users.noreply.github.com>
2021-09-09 22:26:27 +0300

    Add "Sum of two lowest positive integers" kata

---
+++ src/03_Sum of two lowest positive integers.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/558fc85d8fd1938afb000014
+
+function sumTwoSmallestNumbers(numbers) {  
+    numbers = numbers.sort((a, b) => a - b);
+    return numbers[0] + numbers[1];
+}
